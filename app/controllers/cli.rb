fdce37e667b445398fe8be6377c7b68a7788d462
Alex Boring <alexboring@hotmail.com>
2017-05-09 17:34:49 -0400

    Create front page display

--- app/controllers/cli.rb
+++ app/controllers/cli.rb
@@ -2,9 +2,26 @@ class CLI
 
   def self.run
     puts "Welcome to CLI reddit"
-    posts = RedditApi.get_hash_of_top_posts
-    RedditApi.import_posts_to_db(posts)
+    puts "Here's what's trending right now"
+    sleep(2.0)
+    system("clear")
+    # posts = RedditApi.get_hash_of_top_posts
+    # RedditApi.import_posts_to_db(posts)
+    self.front_page
   end
+
+  def self.front_page
+    Post.all.each_with_index do |post, i|
+     entry =  <<-heredoc
+          --------------------------------------------
+         #{i}. #{post.title}
+               by #{post.author}    submitted on #{post.created_at}
+      heredoc
+      puts entry
+    end
+  end
+
+
 
 end
 
