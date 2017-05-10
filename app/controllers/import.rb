436a8d9706c9effd38d70d37102e428bdb853874
Alex Boring <alexboring@hotmail.com>
2017-05-10 13:35:42 -0400

    do nothing

--- app/controllers/import.rb
+++ app/controllers/import.rb
@@ -1,18 +1,16 @@
 class Import
-
-
   def self.to_database
     post_objects = posts_to_database
     comments_to_database(post_objects)
   end
 
-  
-  private 
+  private
 
+  #returns posts as Post objects
   def self.posts_to_database
     posts = RedditApi.get_hash_of_top_posts
     posts.map do |post|
-      if Post.find_by(post_id: post[:post_id]).nil? 
+      if Post.find_by(post_id: post[:post_id]).nil?
         Post.create(post)
       else
         Post.find_by(post_id: post[:post_id])
@@ -21,11 +19,12 @@ class Import
   end
 
 
+
   def self.comments_to_database(post_objects, num_comments=25)
     comments = post_objects.map { |post| RedditApi.get_hash_of_comments(post) }
-    comments.map do |comments_page| 
+    comments.map do |comments_page|
       comments_page.map do |comment|
-        if Comment.find_by(comment_id: comment[:comment_id]).nil? 
+        if Comment.find_by(comment_id: comment[:comment_id]).nil?
           Comment.create(comment)
         else
           Comment.find_by(comment_id: comment[:comment_id])
@@ -33,9 +32,5 @@ class Import
       end
     end
   end
+
 end
-
-  
-
-
-
