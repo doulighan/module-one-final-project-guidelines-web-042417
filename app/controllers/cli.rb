29d00dbd4e8040f75d99abd8e8217f1e9a77baa9
doulighan <doulighan@gmail.com>
2017-05-11 15:33:18 -0400

    refactoring cli

--- app/controllers/cli.rb
+++ app/controllers/cli.rb
@@ -1,23 +1,54 @@
 class CLI
+  
+  attr_accessor :comment, :comments, :post, :posts, :input, :subreddit
+
+  def initialize
+    @comment
+    @comments 
+    @posts
+    @post
+    @input
+    @subreddit
+
+    self.homepage
+    @input = self.get_input
+  end
+
+  
   def self.run
-   
     #Import.to_database
+    homepage
+    input = get_input
+  end
+
+
+  def homepage
     Display.welcome
-    posts = Display.top_posts
+    @posts = Display.top_posts
+    puts " "
+    puts "************* CURRENTLY VIEWING: FRONT PAGE *************"
+    puts " "
+    puts "Enter post number (1-10) to view its comments" 
+    puts "Enter (q) to quit "
+    puts " "
+  end
 
-    
+  def self.handle_input(input)
+    case input
+      where "q"
+        CLI.exit
+      where %w(1 2 3 4 5 6 7 8 9 10).include?(input)
+        input.to_i
+      where
+        input 
+    end
+  end
 
+  
 
 
 
     while(true)
-      puts " "
-      puts "************* CURRENTLY VIEWING: FRONT PAGE *************"
-      puts " "
-      puts "Enter post number (1-10) to view its comments" 
-      puts "Enter (q) to quit "
-      puts " "
-      
       input = gets.chomp.to_i
       exit if input == "q"
       
@@ -54,5 +85,17 @@ class CLI
     end
 
 
+
+
   end
-end
+
+  def self.exit
+    puts " GOODBYE!"
+    abort 
+  end
+
+  def self.get_input
+    gets.strip
+  end
+
+
