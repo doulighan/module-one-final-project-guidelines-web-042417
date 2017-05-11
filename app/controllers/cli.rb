bfc4c7ad753f27e8ebd498c600346883bebf5998
doulighan <doulighan@gmail.com>
2017-05-10 20:26:14 -0400

    working on cli

--- app/controllers/cli.rb
+++ app/controllers/cli.rb
@@ -1,13 +1,29 @@
 class CLI
   def self.run
-    Import.to_database
-    Display.top_posts(20)
-    puts ""
-    puts ""
-    puts "Welcome to Reddit CLI!"
-    puts ""
-    puts ""
-    puts "Enter post number to view comments..."
+    #Import.to_database
+    posts = Display.top_posts(20)
+    
+    Display.welcome
+
+    while(true)
+      puts "Enter post number to view comments or (q) to quit..."
+      input = gets.chomp.to_i
+      if input == "q"
+        break
+      end
+
+      while(true)
+        comments = Display.comments_page(posts, input)
+        puts " "
+        puts "Enter number to expand comment, or (q) to go back to posts"
+        input = gets.chomp.to_i
+        if input == "q"
+          break
+        end
+        comments = Display.expand_comment(comments, input)
+      end
+    end
+
 
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'app'
+
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
+
+#ActiveRecord::Base.logger = old_logger
